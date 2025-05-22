b29cdaa5b87a4c25399d73df953187a0a9642018
I477212 <cbruikman@gmail.com>
2025-05-22 17:02:41 +0200

    Update Tailwind CSS configuration and dependencies; add tailwindcss-animate plugin

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,6 +5,7 @@ export default function HomePage() {
   return (
     <div style={{ padding: '2rem' }}>
       <h1>Welkom bij de TODO App</h1>
+      
       <p>Navigeer naar de <Link to="/todo">Todo-pagina</Link> om te beginnen.</p>
     </div>
   );
